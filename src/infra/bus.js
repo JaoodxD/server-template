04244281600412865110a55a5cb5ebcf812b9784
JaoodxD <jaood96@gmail.com>
2023-07-05 23:51:59 +0300

    add typings to event `bus`

--- src/infra/bus.js
+++ src/infra/bus.js
@@ -9,21 +9,44 @@ module.exports = class Bus {
     this.#services = new Map();
   }
 
+  /**
+   * 
+   * @param {String} eventName 
+   * @param {(any: any[]) => any} handler 
+   * @returns {Boolean}
+   */
   subscribe(eventName, handler) {
     this.#ee.on(eventName, handler);
     return true;
   }
 
+  /**
+   * 
+   * @param {string} eventName 
+   * @param {object?} payload 
+   * @returns {boolean}
+   */
   publish(eventName, payload) {
     return this.#ee.emit(eventName, payload);
   }
 
+  /**
+   * 
+   * @param {string} serviceName 
+   * @param {object?} payload 
+   * @returns {any}
+   */
   command(serviceName, payload) {
     const service = this.#services.get(serviceName);
     if (!service) throw new Error(`No such service: ${serviceName}`);
     return service(payload ?? {});
   }
 
+  /**
+   * 
+   * @param {string} name 
+   * @param {(any: any[]) => any} service 
+   */
   registerService(name, service) {
     this.#services.set(name, service);
   }
