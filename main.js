2e8ae3b8b167bb973c60d3cc3210e67cbae3e05b
JaoodxD <jaood96@gmail.com>
2023-07-05 23:43:10 +0300

    add entry point

---
+++ main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const app = require('./src/app.js');
+
+console.log('Entry point is here');
+app();
---
+++ src/app.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const app = async () => console.log('App is running...');
+
+module.exports = app;
