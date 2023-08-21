2d583a43174c683dcbfc2569772ff170c5326c44
JaoodxD <jaood96@gmail.com>
2023-08-21 21:21:49 +0300

    Add test `routes` and `fallback` function

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 'use strict';
 
-const app = async () => console.log('App is running...');
+const http = require('./infra/http.js');
+
+const app = async () => {
+  console.log('App is running...');
+
+  const routes = {
+    '': () => 'hello, world!',
+    'time': () => new Date()
+  };
+  const fallback = () => 'WRONG PLACE TO SNEAK :)';
+  const server = http({ routes, fallback });
+}
 
 module.exports = app;
